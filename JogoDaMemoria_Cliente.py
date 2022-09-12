fdb49d885cd77de1f6fe2c8cde35f536b87006c5
BGallo <bgallo@id.uff.br>
2022-09-12 19:20:39 -0300

    OK

--- JogoDaMemoria_Cliente.py
+++ JogoDaMemoria_Cliente.py
@@ -1,14 +1,9 @@
-from email import message
-from lib2to3.pgen2.token import RPAR
 import os
 import sys
-import time
 import threading
-from multiprocessing import process
 import random
 import socket
 import json
-from io import StringIO
 
 ##
 # Funcoes uteis
@@ -230,26 +225,6 @@ def leCoordenada(dim,entry):
 ##
 # Parametros da partida
 ##
-'''
-# Tamanho (da lateral) do tabuleiro. NECESSARIAMENTE PAR E MENOR QUE 10!
-dim = 4
-
-# Numero de jogadores
-nJogadores = 2
-
-# Numero total de pares de pecas
-totalDePares = dim**2 / 2
-
-##
-# Programa principal
-##
-
-# Cria um novo tabuleiro para a partida
-tabuleiro = novoTabuleiro(dim)
-
-# Cria um novo placar zerado
-placar = novoPlacar(nJogadores)
-'''
 class client_lock():
     def __init__(self):
         self.gameStarted = False
@@ -279,23 +254,7 @@ def client_send(client, client_lock: client_lock):
         else:
             print("Chat desabilitado!")
     sys.exit(0)
-'''
-def decodeArray(array):
-    array = array.split("%")
-    array.pop(len(array) - 1)
-    result = []
-    for line in array:
-        line = line.replace("[", "")
-        line = line.replace("]", "")
-        line = line.replace("'", "")
-        line = line.replace(" ", "")
-        elem = line.split(",")
-        for i in range(len(elem)):
-            if(elem[i] != "-"):
-                elem[i] = int(elem[i])
-        result.append(elem)
-    return result
-'''
+
 
 host = input("Digite o IP do servidor: ")
 port = int(input("Digite a porta do servidor: "))
@@ -339,102 +298,3 @@ while True:
             tcp_client.close()
             break
 
-'''
-# Partida continua enquanto ainda ha pares de pecas a 
-# casar.
-paresEncontrados = 0
-vez = 0
-while paresEncontrados < totalDePares:
-
-    # Requisita primeira peca do proximo jogador
-    while True:
-
-        # Imprime status do jogo
-        imprimeStatus(tabuleiro, placar, vez)
-
-        # Solicita coordenadas da primeira peca.
-        coordenadas = leCoordenada(dim)
-        if coordenadas == False:
-            continue
-
-        i1, j1 = coordenadas
-
-        # Testa se peca ja esta aberta (ou removida)
-        if abrePeca(tabuleiro, i1, j1) == False:
-
-            print ("Escolha uma peca ainda fechada!")
-            input("Pressione <enter> para continuar...")
-            continue
-
-        break 
-
-    # Requisita segunda peca do proximo jogador
-    while True:
-
-        # Imprime status do jogo
-        imprimeStatus(tabuleiro, placar, vez)
-
-        # Solicita coordenadas da segunda peca.
-        coordenadas = leCoordenada(dim)
-        if coordenadas == False:
-            continue
-
-        i2, j2 = coordenadas
-
-        # Testa se peca ja esta aberta (ou removida)
-        if abrePeca(tabuleiro, i2, j2) == False:
-
-            print ("Escolha uma peca ainda fechada!")
-            input("Pressione <enter> para continuar...")
-            continue
-
-        break 
-
-    # Imprime status do jogo
-    imprimeStatus(tabuleiro, placar, vez)
-
-    print ("Pecas escolhidas --> ({0}, {1}) e ({2}, {3})\n".format(i1, j1, i2, j2))
-
-    # Pecas escolhidas sao iguais?
-    if tabuleiro[i1][j1] == tabuleiro[i2][j2]:
-
-        print ("Pecas casam! Ponto para o jogador {0}.".format(vez + 1))
-        
-        incrementaPlacar(placar, vez)
-        paresEncontrados = paresEncontrados + 1
-        removePeca(tabuleiro, i1, j1)
-        removePeca(tabuleiro, i2, j2)
-
-        time.sleep(5)
-    else:
-
-        print("Pecas nao casam!")
-        
-        time.sleep(3)
-
-        fechaPeca(tabuleiro, i1, j1)
-        fechaPeca(tabuleiro, i2, j2)
-        vez = (vez + 1) % nJogadores
-
-# Verificar o vencedor e imprimir
-pontuacaoMaxima = max(placar)
-vencedores = []
-for i in range(0, nJogadores):
-
-    if placar[i] == pontuacaoMaxima:
-        vencedores.append(i)
-
-if len(vencedores) > 1:
-
-    sys.stdout.write("Houve empate entre os jogadores ")
-    for i in vencedores:
-        sys.stdout.write(str(i + 1) + ' ')
-
-    sys.stdout.write("\n")
-
-else:
-
-    print("Jogador {0} foi o vencedor!".format(vencedores[0] + 1))
-
-
-'''
--- JogoDaMemoria_Servidor.py
+++ JogoDaMemoria_Servidor.py
@@ -1,13 +1,9 @@
-from ipaddress import ip_address
-from multiprocessing import connection
-from multiprocessing.connection import wait
 import os
 import sys
 import time
 import threading
-import types
-import random
 import socket
+import random
 import json
 # Teste
 
@@ -329,14 +325,6 @@ class gameInstance():
         self.reset()
         receive(self.socket,self.clients,self.ids,self)
 
-'''
-def encodeArray(array):
-    line = ""
-    for i in array:
-        line += str(i) + "%"
-    return line
-'''
-
 # Função padrão que manda uma mensagem para todos os clientes
 def sendMessageToClients(message,clients):
     for client in clients:
@@ -424,102 +412,3 @@ def main():
     receive(tcp_server,client_list,ids,game)
 
 main()
-
-'''
-# Partida continua enquanto ainda ha pares de pecas a 
-# casar.
-paresEncontrados = 0
-vez = 0
-while paresEncontrados < totalDePares:
-
-    # Requisita primeira peca do proximo jogador
-    while True:
-
-        # Imprime status do jogo
-        imprimeStatus(tabuleiro, placar, vez)
-
-        # Solicita coordenadas da primeira peca.
-        coordenadas = leCoordenada(dim)
-        if coordenadas == False:
-            continue
-
-        i1, j1 = coordenadas
-
-        # Testa se peca ja esta aberta (ou removida)
-        if abrePeca(tabuleiro, i1, j1) == False:
-
-            print ("Escolha uma peca ainda fechada!")
-            input("Pressione <enter> para continuar...")
-            continue
-
-        break 
-
-    # Requisita segunda peca do proximo jogador
-    while True:
-
-        # Imprime status do jogo
-        imprimeStatus(tabuleiro, placar, vez)
-
-        # Solicita coordenadas da segunda peca.
-        coordenadas = leCoordenada(dim)
-        if coordenadas == False:
-            continue
-
-        i2, j2 = coordenadas
-
-        # Testa se peca ja esta aberta (ou removida)
-        if abrePeca(tabuleiro, i2, j2) == False:
-
-            print ("Escolha uma peca ainda fechada!")
-            input("Pressione <enter> para continuar...")
-            continue
-
-        break 
-
-    # Imprime status do jogo
-    imprimeStatus(tabuleiro, placar, vez)
-
-    print ("Pecas escolhidas --> ({0}, {1}) e ({2}, {3})\n".format(i1, j1, i2, j2))
-
-    # Pecas escolhidas sao iguais?
-    if tabuleiro[i1][j1] == tabuleiro[i2][j2]:
-
-        print ("Pecas casam! Ponto para o jogador {0}.".format(vez + 1))
-        
-        incrementaPlacar(placar, vez)
-        paresEncontrados = paresEncontrados + 1
-        removePeca(tabuleiro, i1, j1)
-        removePeca(tabuleiro, i2, j2)
-
-        time.sleep(5)
-    else:
-
-        print("Pecas nao casam!")
-        
-        time.sleep(3)
-
-        fechaPeca(tabuleiro, i1, j1)
-        fechaPeca(tabuleiro, i2, j2)
-        vez = (vez + 1) % nJogadores
-
-# Verificar o vencedor e imprimir
-pontuacaoMaxima = max(placar)
-vencedores = []
-for i in range(0, nJogadores):
-
-    if placar[i] == pontuacaoMaxima:
-        vencedores.append(i)
-
-if len(vencedores) > 1:
-
-    sys.stdout.write("Houve empate entre os jogadores ")
-    for i in vencedores:
-        sys.stdout.write(str(i + 1) + ' ')
-
-    sys.stdout.write("\n")
-
-else:
-
-    print("Jogador {0} foi o vencedor!".format(vencedores[0] + 1))
-
-'''
